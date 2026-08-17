227b324d84b2d422a00b8a5421e2cd5ad4b1af2a
setiawanhennie-glitch <setiawanhennie@gmail.com>
2026-08-17 21:35:57 +0700

    Added ContactModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoursesModule } from './courses/courses.module';
 import { AuthModule } from './auth/auth.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [CoursesModule, AuthModule],
+  imports: [CoursesModule, AuthModule, ContactModule],
 })
 export class AppModule {}
